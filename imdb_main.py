fd31fe4cfe7e923e42caf71ee2250f5b227bee96
Rushabh Rumde <66670321+rushabhrumde@users.noreply.github.com>
2021-05-08 13:07:36 +0530

    Update imdb_main.py

--- imdb_main.py
+++ imdb_main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 import tensorflow as tf
 app = FastAPI()
 
-model = tf.keras.models.load_model('/Users/rushabhrumde/Desktop/GitHub/Flask/DEMD_Project/tf_keras_imdb')
+model = tf.keras.models.load_model('tf_keras_imdb')
 
 
 class Reviews(BaseModel):
